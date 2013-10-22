0b5f31bcf479b983ddfbdff4c82edc82b02c3b40
Zheng Yan <yzheng624@gmail.com>
2013-10-22 15:22:56 +0800

    tweak license.

--- main.d
+++ main.d
@@ -4,7 +4,7 @@ import interp;
 void main(string args[]) {
   writeln("Hello, world.");
   writeln("Welcome to the Chikatetsu monitor. Commands end with ;.");
-  writeln("Server version: 0.0.1 ChikatetsuLuDa (GPL)");
+  writeln("Server version: 0.0.1 ChikatetsuLuDa (MIT)");
 	writeln("Copyright (c) 2013, TeaWhen and/or its affiliates. All rights reserved.");
 
 	handler();
