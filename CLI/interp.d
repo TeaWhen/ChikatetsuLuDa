69543eddd3759d1ffcbdeae557f5a94df8519dc8
Zheng Yan <yzheng624@gmail.com>
2013-10-23 16:58:01 +0800

    Implement execfile.

--- CLI/interp.d
+++ CLI/interp.d
@@ -2,6 +2,8 @@ import std.string;
 import std.stdio;
 import std.regex;
 import std.conv;
+import std.c.process;
+import std.file;
 
 import API.table;
 import API.record;
@@ -16,7 +18,7 @@ auto select_reg = regex(r"^select \* from ([a-z]+);$");
 auto insert_reg = regex(r"^insert into ([a-z]+) values;$");
 auto delete_reg = regex(r"^delete from ([a-z]+);$");
 auto quit_reg = regex(r"^(quit)|(exit);$");
-auto execfile_reg = regex(r"^execfile ([a-zA-Z0-9._-]+);$");
+auto execfile_reg = regex(r"^execfile ([a-zA-Z0-9./_]+);$");
 
 auto column_reg = regex(r"([a-z]+) ([a-z]+)(?: \(([0-9]+)\))?( unique)?,",  "g");
 auto primary_key_reg = regex(r"primary key \( ([a-z]+) \)");
@@ -24,35 +26,42 @@ auto primary_key_reg = regex(r"primary key \( ([a-z]+) \)");
 void handler() {
 	while (true) {
     write("chikatetsu> ");
-		string input;
+		string input = get_input(stdin);
+    _handler(input);
+	}
+}
 
-    foreach (line_raw; stdin.byLine()) {
-      string line = to!string(strip(line_raw));
-      if (line == "") {
-        break;
-      }
-      if (input == "") {
-        input = line;
+string get_input(File f) {
+  string input;
+  foreach (line_raw; f.byLine()) {
+    string line = to!string(strip(line_raw));
+    if (line == "") {
+      break;
+    }
+    if (input == "") {
+      input = line;
+    }
+    else {
+      if (line != ";") {
+        input = format("%s %s", input, line);
       }
       else {
-        if (line != ";") {
-          input = format("%s %s", input, line);
-        }
-        else {
-          input = format("%s%s", input, line);
-        }
+        input = format("%s%s", input, line);
       }
-      if (endsWith(input, ";")) {
-        break;
-      }
-      // write("chikatetsu. ");
     }
+    if (endsWith(input, ";")) {
+      break;
+    }
+    // write("chikatetsu. ");
+  }
+  return input;
+}
 
+void _handler(string input) {
     if (input == "") {
-      continue;
+      return;
     }
-
-    if (match(input, create_table_reg)) {
+    else if (match(input, create_table_reg)) {
       auto m = match(input, create_table_reg);
       string table_name = m.captures[1];
       string scheme = format("%s,", strip(m.captures[2]));
@@ -106,14 +115,14 @@ void handler() {
       }
 
       create_table(table_name, cols, pk);
-      continue;
+      return;
     }
 
     if (match(input, drop_table_reg)) {
       auto m = match(input, drop_table_reg);
       string table_name = m.captures[1];
       drop_table(table_name);
-      continue;
+      return;
     }
 
     if (match(input, create_index_reg)) {
@@ -122,54 +131,62 @@ void handler() {
       string table_name = m.captures[2];
       string column_name = m.captures[3];
       create_index(index_name, table_name, column_name);
-      continue;
+      return;
     }
 
     if (match(input, drop_index_reg)) {
       auto m = match(input, drop_index_reg);
       string index_name = m.captures[1];
       drop_index(index_name);
-      continue;
+      return;
     }
 
     if (match(input, select_reg)) {
       auto m = match(input, select_reg);
       string table_name = m.captures[1];
       select_record(table_name);
-      continue;
+      return;
     }
 
     if (match(input, insert_reg)) {
       auto m = match(input, insert_reg);
       string table_name = m.captures[1];
       insert_record(table_name);
-      continue;
+      return;
     }
 
     if (match(input, delete_reg)) {
       auto m = match(input, delete_reg);
       string table_name = m.captures[1];
       delete_record(table_name);
-      continue;
+      return;
     }
 
     if (match(input, quit_reg)) {
-      break;
+      writeln("Bye.");
+      exit(0);
     }
 
     if (match(input, execfile_reg)) {
       auto m = match(input, execfile_reg);
       string file_name = m.captures[1];
       execfile(file_name);
-      continue;
+      return;
     }
 
     if (input) {
       writeln("something is wrong.");
     }
-	}
 }
 
 void execfile(string file_name) {
-  writeln("loading file ", file_name);
+  if (exists(file_name)) {
+    // TODO support many statments in a single file
+    auto f = File(file_name, "r");
+    string input = get_input(f);
+    _handler(input);
+  }
+  else {
+    writeln(file_name, " does not exists.");
+  }
 }
--- main.d
+++ main.d
@@ -5,6 +5,4 @@ void main(string args[]) {
   writeln("Welcome to the Chikatetsu monitor. Command ends with “;”.");
 
   handler();
-
-  writeln("Bye.");
 }
